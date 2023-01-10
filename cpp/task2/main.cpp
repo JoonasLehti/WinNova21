133bb6f7fc92448e4d8384ddfa23367da215f9ea
JoonasLehti <joonas.a71839@student.winnova.fi>
2023-01-10 20:03:46 +0200

    vaiheet 2. ja 3.
    päivämäärä annetaan suomalaisessa muodossa "31.12.1234" jonk koodi tarkistaa onko se kelvollinen päivä, jos on niin se kertoo onko se karkausvuosi vai ei

--- cpp/task2/main.cpp
+++ cpp/task2/main.cpp
@@ -2,19 +2,79 @@
 
 using namespace std;
 
-int main()
+bool Karkausvuosi(int vuosi)
 {
-    int vuosi;
-
-    cout << "Anna vuosiluku: " << endl;
-    cin >> vuosi;
-
     if ((vuosi % 4 == 0 && vuosi % 100 != 0) || vuosi % 400 == 0)
     {
-        cout << "karkausvuosi" << endl;
+        return true;
     }
     else
     {
-        cout << "Ei ole karkausvuosi" << endl;
+        return false;
     }
+}
+
+bool paivamaaraTark(int &paiva, int &kuukausi, int &vuosi)
+{
+    if (kuukausi < 1 || kuukausi > 12)
+    {
+        return false;
+    }
+
+    if (vuosi < 0)
+    {
+        return false;
+    }
+
+    int KuukaudenPaivat = 31;
+    if (kuukausi == 2)
+    {
+        if ((vuosi % 4 == 0 && vuosi % 100 != 0) || vuosi % 400 == 0)
+        {
+            KuukaudenPaivat = 29;
+        }
+        else
+        {
+            KuukaudenPaivat = 28;
+        }
+    }
+    else if (kuukausi == 4 || kuukausi == 6 || kuukausi == 9 || kuukausi == 11)
+    {
+        KuukaudenPaivat = 30;
+    }
+    if (paiva < 1 || paiva > KuukaudenPaivat)
+    {
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    string teksti;
+
+    cout << "Anna paivamaara muodossa 31.12.1234" << endl;
+    cin >> teksti;
+
+    int paiva = stoi(teksti.substr(0, 2));
+    int kuukausi = stoi(teksti.substr(3, 2));
+    int vuosi = stoi(teksti.substr(6));
+
+    if (paivamaaraTark(paiva, kuukausi, vuosi))
+    {
+        cout << "paivamaara kelpaa" << endl;
+        if (Karkausvuosi(vuosi))
+        {
+            cout << "on karkausvuosi" << endl;
+        }
+        else
+        {
+            cout << "ei ole karkausvuosi" << endl;
+        }
+    }
+    else
+    {
+        cout << "paivamaara ei kelpaa." << endl;
+    }
+        return 0;
 }
