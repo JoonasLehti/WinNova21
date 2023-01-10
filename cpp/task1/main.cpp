e378b529c8b1800d2195843d9abc41b23e25db7a
JoonasLehti <joonas.a71839@student.winnova.fi>
2023-01-10 17:27:10 +0200

    koodi ja funktiot kirjoitettu

--- cpp/task1/main.cpp
+++ cpp/task1/main.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
+void AnnaLuku(int &luku1, int &luku2)
+{
+    cout << "Anna ensimmainen luku: ";
+    cin >> luku1;
+    cout << "Anna toinen luku: ";
+    cin >> luku2;
+}
+
+void TulostaLuvut(int luku1, int luku2)
+{
+    cout << "Lukujen summa on: " << luku1 + luku2 << endl;
+    cout << "Lukujen erotus on: " << luku1 - luku2 << endl;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int x, y;
+    AnnaLuku(x, y);
+    TulostaLuvut(x, y);
     return 0;
 }
