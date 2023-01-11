aab58b7f943f3726dea533f2b97585461fe6bd6c
JoonasLehti <joonas.a71839@student.winnova.fi>
2023-01-11 17:29:28 +0200

    task3 vaihe 3.
    Koodin vähän sekava kun yritin kehitellä miten saan nuo työntekijät lajiteltua iän mukaan ja tulostettua kokonaan, mutta nyt koodi tulostaa rivien määrän, miesten ja naisten määrän ja vanhimmat henkilöt syntymäpäivän mukaan kokonaan tietoineen.

--- cpp/task3/main.cpp
+++ cpp/task3/main.cpp
@@ -3,13 +3,27 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <algorithm>
+#include <iomanip>
 
 using namespace std;
+
+struct date {
+    int year;
+    int month;
+    int day;
+};
 
 int main()
 {
     int miehet = 0;
     int naiset = 0;
+
+    vector<int> vuosi;
+    vector<int> kuukaudet;
+    vector<int> paivat;
+    vector<date> dates;
+
     vector<string> rivit;
     string teksti;
 
@@ -17,6 +31,8 @@ int main()
 
     if (tiedosto.is_open())
     {
+
+
         while (getline (tiedosto, teksti))
         {
             rivit.push_back(teksti);
@@ -24,6 +40,7 @@ int main()
             stringstream ssin(teksti);
             string osat[5];
             int i = 0;
+
             while(getline(ssin, osat[i], ','))
             {
                 i++;
@@ -36,15 +53,58 @@ int main()
             {
                 naiset++;
             }
+            string syntyma = osat[3];
+
+            string vuos = syntyma.substr(0, 4);
+            int year = (stoi(vuos));
+
+            string kk = syntyma.substr(5, 2);
+            int month = (stoi(kk));
+
+            string pv = syntyma.substr(8, 2);
+            int day = (stoi(pv));
+
+            date dt = {year, month, day};
+            dates.push_back(dt);
+
+
         }
+
+        cout << "Rivien maara: " << rivit.size() << endl;
+        cout << "Miehia: " << miehet << endl;
+        cout << "Naisia: " << naiset << endl;
+
+        cout << endl;
+        cout << "Vanhin tyontekija: " << endl;
+
+        sort(dates.begin(), dates.end(), [](const date &a, const date &b) {
+            return a.year < b.year || (a.year == b.year && (a.month < b.month || (a.month == b.month && a.day < b.day)));
+        });
+
+        char buffer[20];
+        sprintf(buffer, "%04d-%02d-%02d", dates[0].year, dates[0].month, dates[0].day);
+
         tiedosto.close();
+        tiedosto.open("C:\\Users\\Lennu\\Documents\\GitHub\\Joonas_Lehti\\cpp\\task3\\test.csv");
+
+            if (tiedosto.is_open())
+            {
+                while (getline (tiedosto, teksti))
+                {
+                    if (teksti.find(buffer, 0) != string::npos)
+                    {
+                        cout << teksti << endl;
+                    }
+                }
+                tiedosto.close();
+            }
+            else
+            {
+                cout << "Tiedostoa ei ole" << endl;
+            }
     }
     else
     {
         cout << "Tiedostoa ei ole" << endl;
     }
-    cout << rivit.size() << endl;
-    cout << "Miehia: " << miehet << endl;
-    cout << "Naisia: " << naiset << endl;
 }
-
