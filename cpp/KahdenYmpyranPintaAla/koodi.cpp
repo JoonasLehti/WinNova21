73ce8fea13d2a5e5f8f4286ceb662f4d6aaaca13
JoonasLehti <joonas.a71839@student.winnova.fi>
2023-03-20 20:54:20 +0200

    Update koodi.cpp

--- cpp/KahdenYmpyranPintaAla/koodi.cpp
+++ cpp/KahdenYmpyranPintaAla/koodi.cpp
@@ -6,7 +6,7 @@
 Koodi::Koodi()
 {
     std::mt19937 generator{42};
-    std::uniform_real_distribution<double> distribution_x(-28.2, 11.8);
+    std::uniform_real_distribution<double> distribution_x(-20.0, 20.0);
     std::uniform_real_distribution<double> distribution_y(-20.0, 20.0);
 
     const int num_yrityksia = 1000000;
@@ -16,19 +16,19 @@ Koodi::Koodi()
     {
         double x = distribution_x(generator);
         double y = distribution_y(generator);
-        double matka_keskelta = std::sqrt(x*x + y*y);
+        double matka_keskelta1 = std::sqrt(x*x + y*y);
+        double matka_keskelta2 = std::sqrt((x-8)*(x-8) + y*y);
 
-        if (matka_keskelta <= 20.0 && matka_keskelta >= 16.2 && x >= -8.0)
+        if (matka_keskelta1 <= 20.0 && matka_keskelta2 > 16.2)
         {
             num_sininen_piste++;
         }
     }
 
     double nelion_leveys = 40.0;
-    double nelion_korkeus = 32.4;
+    double nelion_korkeus = 40.0;
     double nelion_ala = nelion_leveys * nelion_korkeus;
     double sinisen_ympyran_alue = nelion_ala * static_cast<double>(num_sininen_piste) / static_cast<double>(num_yrityksia);
 
-    std::cout << "sinisen alueen koko on " << sinisen_ympyran_alue << " neliometria" << std::endl;
+    std::cout << "Sinisen alue on " << sinisen_ympyran_alue << " neliometria" << std::endl;
 }
-
