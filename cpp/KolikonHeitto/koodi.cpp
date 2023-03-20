4744fc021895a95a3b1d16adeefb739eb45343dc
JoonasLehti <joonas.a71839@student.winnova.fi>
2023-03-20 19:56:23 +0200

    Update koodi.cpp

--- cpp/KolikonHeitto/koodi.cpp
+++ cpp/KolikonHeitto/koodi.cpp
@@ -7,7 +7,7 @@ koodi::koodi()
     std::mt19937 generator{ 42 };
     std::uniform_int_distribution<int> distribution(0, 1);
 
-    const int num_yrityksia = 1000000;
+    const int num_yrityksia = 1000000000;
         int num_perkkaiset_klaavat = 0;
         int num_perkkaiset_kruunat = 0;
         int num_kymmenen_perkkaiset_klaavat = 0;
@@ -20,7 +20,7 @@ koodi::koodi()
             {
                 num_perkkaiset_klaavat++;
                 num_perkkaiset_kruunat = 0;
-                if (num_perkkaiset_klaavat == 10)
+                if (num_perkkaiset_klaavat == 9)
                 {
                     num_kymmenen_perkkaiset_klaavat++;
                     num_perkkaiset_klaavat = 0;
@@ -30,7 +30,7 @@ koodi::koodi()
             {
                 num_perkkaiset_kruunat++;
                 num_perkkaiset_klaavat = 0;
-                if (num_perkkaiset_kruunat == 10)
+                if (num_perkkaiset_kruunat == 9)
                 {
                     num_kymmenen_perkkaiset_kruunat++;
                     num_perkkaiset_kruunat = 0;
@@ -38,6 +38,6 @@ koodi::koodi()
             }
         }
 
-        double probability = static_cast<double>(num_kymmenen_perkkaiset_klaavat) / (num_kymmenen_perkkaiset_klaavat + num_kymmenen_perkkaiset_kruunat);
+        double probability = static_cast<double>(num_kymmenen_perkkaiset_klaavat) / num_yrityksia;
         std::cout << "mahdollisuus saada 10 klaavaa perakkain: " << probability << std::endl;
 }
