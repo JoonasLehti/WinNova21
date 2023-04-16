ed29ca8bca99e0422471bce5426ee5dd528498a6
JoonasLehti <joonas.a71839@student.winnova.fi>
2023-04-16 16:07:58 +0300

    HakuLompakosta3

---
+++ cpp/HakuLompakosta3/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+struct CompareKeys {
+    bool operator()(const string& lhs, const string& rhs) const {
+        double lhsValue = stod(lhs.substr(0, lhs.find(' ')));
+        double rhsValue = stod(rhs.substr(0, rhs.find(' ')));
+        if (lhs.find("cent") != string::npos) {
+            lhsValue /= 100;
+        }
+        if (rhs.find("cent") != string::npos) {
+            rhsValue /= 100;
+        }
+        return lhsValue < rhsValue;
+    }
+};
+
+int main() {
+    map<string, int, CompareKeys> values = {
+        {"1 cent", 5},
+        {"2 cent", 3},
+        {"5 cent", 5},
+        {"10 cent", 3},
+        {"50 cent", 7},
+        {"1 €", 5},
+        {"2 €", 3},
+        {"5 €", 5},
+        {"10 €", 1},
+        {"20 €", 5},
+        {"50 €", 8},
+        {"100 €", 3}
+        };
+
+    try {
+        cout << "20 centin kolikoita:  " << values.at("20 cent") << endl;
+    } catch (const out_of_range& e) {
+        cout << "ei loydy" << endl;
+    }
+
+    try {
+        cout << "50 centin kolikoita:  " << values.at("50 cent") << endl;
+    } catch (const out_of_range& e) {
+        cout << "ei loydy" << endl;
+    }
+
+    return 0;
+}
