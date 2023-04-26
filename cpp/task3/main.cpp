9e6e9533d3f4687b488171a9cfdbf045e8171a88
JoonasLehti <joonas.a71839@student.winnova.fi>
2023-04-26 21:44:03 +0300

    employees luokka luotu

--- cpp/task3/main.cpp
+++ cpp/task3/main.cpp
@@ -14,26 +14,49 @@ struct date {
     int day;
 };
 
+class Employee {
+public:
+    Employee(string name, string gender, int year, int month, int day) {
+        this->name = name;
+        this->gender = gender;
+        this->dob.year = year;
+        this->dob.month = month;
+        this->dob.day = day;
+    }
+
+    string getName() const {
+        return name;
+    }
+
+    string getGender() const {
+        return gender;
+    }
+
+    date getDOB() const {
+        return dob;
+    }
+
+private:
+    string name;
+    string gender;
+    date dob;
+};
+
 int main()
 {
     int miehet = 0;
     int naiset = 0;
 
-    vector<int> vuosi;
-    vector<int> kuukaudet;
-    vector<int> paivat;
-    vector<date> dates;
+    vector<Employee> employees;
 
     vector<string> rivit;
     string teksti;
 
-    ifstream tiedosto("C:\\Users\\Lennu\\Documents\\GitHub\\Joonas_Lehti\\cpp\\task3\\test.csv");
+    ifstream tiedosto("test.csv");
 
     if (tiedosto.is_open())
     {
-
-
-        while (getline (tiedosto, teksti))
+        while (getline(tiedosto, teksti))
         {
             rivit.push_back(teksti);
 
@@ -45,29 +68,29 @@ int main()
             {
                 i++;
             }
-            if (osat[2] == "M")
-            {
-                miehet++;
-            }
-            else if (osat[2] == "F")
-            {
-                naiset++;
-            }
-            string syntyma = osat[3];
 
+            string name = osat[0];
+            string gender = osat[2];
+
+            string syntyma = osat[3];
             string vuos = syntyma.substr(0, 4);
             int year = (stoi(vuos));
-
             string kk = syntyma.substr(5, 2);
             int month = (stoi(kk));
-
             string pv = syntyma.substr(8, 2);
             int day = (stoi(pv));
 
-            date dt = {year, month, day};
-            dates.push_back(dt);
+            Employee employee(name, gender, year, month, day);
+            employees.push_back(employee);
 
-
+            if (gender == "M")
+            {
+                miehet++;
+            }
+            else if (gender == "F")
+            {
+                naiset++;
+            }
         }
 
         cout << "Rivien maara: " << rivit.size() << endl;
@@ -77,31 +100,17 @@ int main()
         cout << endl;
         cout << "Vanhin tyontekija: " << endl;
 
-        sort(dates.begin(), dates.end(), [](const date &a, const date &b) {
-            return a.year < b.year || (a.year == b.year && (a.month < b.month || (a.month == b.month && a.day < b.day)));
+        sort(employees.begin(), employees.end(), [](const Employee &a, const Employee &b) {
+            return a.getDOB().year < b.getDOB().year || (a.getDOB().year == b.getDOB().year && (a.getDOB().month < b.getDOB().month || (a.getDOB().month == b.getDOB().month && a.getDOB().day < b.getDOB().day)));
         });
 
-        char buffer[20];
-        sprintf(buffer, "%04d-%02d-%02d", dates[0].year, dates[0].month, dates[0].day);
+        cout << "Name: " << employees[0].getName() << endl;
+        cout << "Gender: " << employees[0].getGender() << endl;
+        cout << "Date of birth: " << setw(4) << setfill('0') << employees[0].getDOB().year << "-"
+             << setw(2) << setfill('0') << employees[0].getDOB().month << "-"
+             << setw(2) << setfill('0') << employees[0].getDOB().day << endl;
 
         tiedosto.close();
-        tiedosto.open("C:\\Users\\Lennu\\Documents\\GitHub\\Joonas_Lehti\\cpp\\task3\\test.csv");
-
-            if (tiedosto.is_open())
-            {
-                while (getline (tiedosto, teksti))
-                {
-                    if (teksti.find(buffer, 0) != string::npos)
-                    {
-                        cout << teksti << endl;
-                    }
-                }
-                tiedosto.close();
-            }
-            else
-            {
-                cout << "Tiedostoa ei ole" << endl;
-            }
     }
     else
     {
