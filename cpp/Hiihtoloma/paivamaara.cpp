d4a22327825383945d6b720c0eb28d868a80fbba
JoonasLehti <joonas.a71839@student.winnova.fi>
2023-04-26 21:26:36 +0300

    update

--- cpp/Hiihtoloma/paivamaara.cpp
+++ cpp/Hiihtoloma/paivamaara.cpp
@@ -1,20 +1,36 @@
 #include "paivamaara.h"
 
-bool Paivamaara::paivamaaraTark(int &paiva, int &kuukausi, int &vuosi)
-{
-    if(kuukausi==2 && vuosi==2023)
-    {
-        if(paiva>=20 && paiva<=26)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else
-    {
-        return false;
-    }
+bool Paivamaara::paivamaaraTark(int& paiva, int& kuukausi, int& vuosi) {
+    Paivamaara alkupvm(20, 2, 2023);
+    Paivamaara loppupvm(26, 2, 2023);
+
+    Paivamaara tarkistettava(paiva, kuukausi, vuosi);
+
+    return (tarkistettava >= alkupvm && tarkistettava <= loppupvm);
+}
+
+bool Paivamaara::operator<(const Paivamaara& other) const {
+    return (vuosi < other.vuosi)
+           || (vuosi == other.vuosi && kuukausi < other.kuukausi)
+           || (vuosi == other.vuosi && kuukausi == other.kuukausi && paiva < other.paiva);
+}
+
+bool Paivamaara::operator<=(const Paivamaara& other) const {
+    return (*this < other) || (*this == other);
+}
+
+bool Paivamaara::operator>(const Paivamaara& other) const {
+    return !(*this <= other);
+}
+
+bool Paivamaara::operator>=(const Paivamaara& other) const {
+    return !(*this < other);
+}
+
+bool Paivamaara::operator==(const Paivamaara& other) const {
+    return (paiva == other.paiva && kuukausi == other.kuukausi && vuosi == other.vuosi);
+}
+
+bool Paivamaara::operator!=(const Paivamaara& other) const {
+    return !(*this == other);
 }
--- cpp/Hiihtoloma/paivamaara.h
+++ cpp/Hiihtoloma/paivamaara.h
@@ -1,11 +1,25 @@
 #ifndef PAIVAMAARA_H
 #define PAIVAMAARA_H
 
-
-class Paivamaara
-{
+class Paivamaara {
 public:
-    bool paivamaaraTark(int &paiva, int &kuukausi, int &vuosi);
+    Paivamaara() : paiva(0), kuukausi(0), vuosi(0) {}
+    Paivamaara(int p, int k, int v) : paiva(p), kuukausi(k), vuosi(v) {}
+
+    bool paivamaaraTark(int& paiva, int& kuukausi, int& vuosi);
+
+    bool operator<(const Paivamaara& other) const;
+    bool operator<=(const Paivamaara& other) const;
+    bool operator>(const Paivamaara& other) const;
+    bool operator>=(const Paivamaara& other) const;
+    bool operator==(const Paivamaara& other) const;
+    bool operator!=(const Paivamaara& other) const;
+
+private:
+    int paiva;
+    int kuukausi;
+    int vuosi;
 };
+
 
 #endif // PAIVAMAARA_H
